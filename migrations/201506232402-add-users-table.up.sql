e6586df2a7171b7f74a8dd02f38d426290cd9e63
manmyung <manmyung@gmail.com>
2015-06-29 23:28:24 +0900

    Creating the Database 까지 따라함

--- migrations/201506232402-add-users-table.up.sql
+++ migrations/201506232402-add-users-table.up.sql
@@ -1,9 +1,5 @@
-CREATE TABLE users
-(id VARCHAR(20) PRIMARY KEY,
- first_name VARCHAR(30),
- last_name VARCHAR(30),
- email VARCHAR(30),
- admin BOOLEAN,
- last_login TIME,
- is_active BOOLEAN,
- pass VARCHAR(100));
+CREATE TABLE guestbook
+(id INTEGER PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(30),
+ message VARCHAR(200),
+ timestamp TIMESTAMP);
