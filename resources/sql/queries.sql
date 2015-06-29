307cdee7146a48766d72e1f75c5b9c7c041e1f3f
manmyung <manmyung@gmail.com>
2015-06-29 23:32:35 +0900

    Accessing The Database 까지 따라함

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,16 +1,9 @@
--- name: create-user!
--- creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+--name:save-message!
+-- creates a new message
+INSERT INTO guestbook
+(name, message, timestamp)
+VALUES (:name, :message, :timestamp)
 
--- name: update-user!
--- update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
-
--- name: get-user
--- retrieve a user given the id.
-SELECT * FROM users
-WHERE id = :id
+--name:get-messages
+-- selects all available messages
+SELECT * from guestbook
