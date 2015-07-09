9d5c8e5ec957992f635d81cb17c51e6e80b74cf0
djey <djey9538@gmail.com>
2015-07-10 02:32:17 +0900

    logout

--- src/guestbook/routes/login.clj
+++ src/guestbook/routes/login.clj
@@ -8,14 +8,19 @@
             [ring.util.response :refer [redirect]])
   (:import (java.util Date)))
 
-(defn login-page []
-  (layout/render "login.html"))
+(defn login-page [{:keys [session]}]
+  (layout/render "login.html" {:session session}))
 
 (defn login! [{:keys [params]}]
   (-> (redirect "/")
       (assoc-in [:session :user-id] (:id params))))
 
+(defn logout! [{:keys [session]}]
+  (-> (redirect "/")
+      (assoc :session (dissoc session :user-id))))
+
 (defroutes login-routes
-           (GET "/login" [] (login-page))
+           (GET "/login" request (login-page request))
            (POST "/login" request (login! request))
+           (POST "/logout" request (logout! request))
            )
